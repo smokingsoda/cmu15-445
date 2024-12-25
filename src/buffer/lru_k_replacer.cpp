a4225a7f498db125738430b418390da6f0f2915b
Junru Wang <junru.wang.24@ucl.ac.uk>
2024-12-25 18:00:09 +0000

    Passed local tests

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -12,7 +12,6 @@
 
 #include "buffer/lru_k_replacer.h"
 #include <cstddef>
-#include <utility>
 #include "common/config.h"
 #include "common/macros.h"
 
@@ -22,13 +21,14 @@ LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_fra
   for (size_t i = 0; i < num_frames; i++) {
     this->is_evitable_.push_back(false);
     this->is_less_than_k_.push_back(true);
-    this->non_evitable_.push_back(i);
+    this->is_init_.push_back(false);
     this->history_.push_back(std::deque<size_t>());
   }
 }
 
 auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
-  if (this->evitable_equal_k_.size() > 0) {
+  std::scoped_lock<std::mutex> lock(this->latch_);
+  if (this->evitable_less_than_k_.size() > 0) {
     size_t earlist_timestamp = std::numeric_limits<size_t>::max();
     frame_id_t target = this->evitable_less_than_k_.front();
     for (auto it = this->evitable_less_than_k_.begin(); it != this->evitable_less_than_k_.end(); ++it) {
@@ -39,6 +39,11 @@ auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
       }
     }
     *frame_id = target;
+    this->is_init_[target] = false;
+    this->evitable_less_than_k_.remove(target);
+    this->history_[target].clear();
+    // this->is_evitable_[target] = false;
+    // this->is_less_than_k_[target] = true;
     return true;
   } else if (this->evitable_equal_k_.size() > 0) {
     size_t max_difference = 0;
@@ -52,6 +57,11 @@ auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
       }
     }
     *frame_id = target;
+    this->evitable_equal_k_.remove(target);
+    this->is_init_[target] = false;
+    this->history_[target].clear();
+    // this->is_evitable_[target] = false;
+    // this->is_less_than_k_[target] = true;
     return true;
   } else {
     return false;
@@ -59,58 +69,72 @@ auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
 }
 
 void LRUKReplacer::RecordAccess(frame_id_t frame_id) {
-    if (this->is_less_than_k_[frame_id]) {
-        this->history_[frame_id].push_front(this->current_timestamp_);
-        if (this->history_[frame_id].size() >= this->k_) {
-            this->is_less_than_k_[frame_id] = false;
-            this->evitable_less_than_k_.remove(frame_id);
-            this->evitable_equal_k_.push_back(frame_id);
-        }
-    } else if (!this->is_less_than_k_[frame_id]) {
-        this->history_[frame_id].push_front(this->current_timestamp_);
-        this->history_[frame_id].pop_back();
-    } else {
-        BUSTUB_ASSERT(true, "No");
+  std::scoped_lock<std::mutex> lock(this->latch_);
+  BUSTUB_ASSERT(frame_id < static_cast<frame_id_t>(this->replacer_size_), "No");
+  if (!this->is_init_[frame_id]) {
+    this->is_init_[frame_id] = true;
+    this->is_evitable_[frame_id] = true;
+    this->is_less_than_k_[frame_id] = true;
+    this->evitable_less_than_k_.push_back(frame_id);
+    this->history_[frame_id].push_front(this->current_timestamp_);
+  } else if (this->is_less_than_k_[frame_id]) {
+    this->history_[frame_id].push_front(this->current_timestamp_);
+    if (this->history_[frame_id].size() >= this->k_) {
+      this->is_less_than_k_[frame_id] = false;
+      this->evitable_less_than_k_.remove(frame_id);
+      this->evitable_equal_k_.push_back(frame_id);
     }
-    this->current_timestamp_ += 1;
+  } else if (!this->is_less_than_k_[frame_id]) {
+    this->history_[frame_id].push_front(this->current_timestamp_);
+    this->history_[frame_id].pop_back();
+  }
+  this->current_timestamp_ += 1;
 }
 
 void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
-    BUSTUB_ASSERT(frame_id >= static_cast<frame_id_t>(this->replacer_size_), "No");
-    if (this->is_evitable_[frame_id] && !set_evictable) {
-        if (this->is_less_than_k_[frame_id]) {
-            this->evitable_less_than_k_.remove(frame_id);
-            this->non_evitable_.push_back(frame_id);
-        } else {
-            this->evitable_equal_k_.remove(frame_id);
-            this->non_evitable_.push_back(frame_id);
-        }
-    } else if (!this->is_evitable_[frame_id] && set_evictable) {
-        if (this->history_[frame_id].size() < this->k_) {
-            this->non_evitable_.remove(frame_id);
-            this->evitable_less_than_k_.push_back(frame_id);
-        } else {
-            this->non_evitable_.remove(frame_id);
-            this->evitable_equal_k_.push_back(frame_id);
-        }
+  std::scoped_lock<std::mutex> lock(this->latch_);
+  BUSTUB_ASSERT(frame_id < static_cast<frame_id_t>(this->replacer_size_), "No");
+  BUSTUB_ASSERT(this->is_init_[frame_id], "No");
+  if (this->is_evitable_[frame_id] && !set_evictable) {
+    if (this->is_less_than_k_[frame_id]) {
+      this->evitable_less_than_k_.remove(frame_id);
+      this->is_evitable_[frame_id] = false;
+    } else {
+      this->evitable_equal_k_.remove(frame_id);
+      this->is_evitable_[frame_id] = false;
     }
+  } else if (!this->is_evitable_[frame_id] && set_evictable) {
+    if (this->history_[frame_id].size() < this->k_) {
+      this->is_evitable_[frame_id] = true;
+      this->evitable_less_than_k_.push_back(frame_id);
+    } else {
+      this->is_evitable_[frame_id] = true;
+      this->evitable_equal_k_.push_back(frame_id);
+    }
+  }
 }
 
 void LRUKReplacer::Remove(frame_id_t frame_id) {
-    if (frame_id > static_cast<frame_id_t>(this->replacer_size_)) {
-        return;
-    }
-    BUSTUB_ASSERT(!this->is_evitable_[frame_id], "No");
+  std::scoped_lock<std::mutex> lock(this->latch_);
+  BUSTUB_ASSERT(frame_id < static_cast<frame_id_t>(this->replacer_size_), "No");
+  BUSTUB_ASSERT(this->is_evitable_[frame_id], "No");
+  if (this->is_init_[frame_id]) {
     this->is_evitable_[frame_id] = false;
-    if (this->is_less_than_k_[frame_id]) {
-        this->evitable_less_than_k_.remove(frame_id);
-        this->non_evitable_.push_back(frame_id);
-    } else {
-        this->evitable_equal_k_.remove(frame_id);
-        this->non_evitable_.push_back(frame_id);
-    }
+  }
+  if (this->is_less_than_k_[frame_id]) {
+    this->evitable_less_than_k_.remove(frame_id);
+    this->is_init_[frame_id] = false;
+    this->history_[frame_id].clear();
+  } else {
+    this->evitable_equal_k_.remove(frame_id);
+    this->is_init_[frame_id] = false;
+    this->history_[frame_id].clear();
+  }
 }
 
-auto LRUKReplacer::Size() -> size_t { return this->replacer_size_ - this->non_evitable_.size(); }
+auto LRUKReplacer::Size() -> size_t {
+  std::scoped_lock<std::mutex> lock(this->latch_);
+  return this->evitable_equal_k_.size() + this->evitable_less_than_k_.size();
+}
 
 }  // namespace bustub
--- src/include/buffer/lru_k_replacer.h
+++ src/include/buffer/lru_k_replacer.h
@@ -137,14 +137,15 @@ class LRUKReplacer {
   // TODO(student): implement me! You can replace these member variables as you like.
   // Remove maybe_unused if you start using them.
   size_t current_timestamp_{0};
-  size_t curr_size_{0};
+  // size_t curr_size_{0};
   size_t replacer_size_;
   size_t k_;
   std::list<frame_id_t> evitable_less_than_k_;
   std::list<frame_id_t> evitable_equal_k_;
-  std::list<frame_id_t> non_evitable_;
+  // std::list<frame_id_t> non_evitable_;
   std::vector<bool> is_less_than_k_;
   std::vector<bool> is_evitable_;
+  std::vector<bool> is_init_;
   std::vector<std::deque<size_t>> history_;
   std::mutex latch_;
 };
