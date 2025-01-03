fc7775b79c623c1caaa67ebda7019896db388b71
Junru Wang <junru.wang.24@ucl.ac.uk>
2025-01-03 09:28:55 +0000

    Revert

--- src/storage/index/b_plus_tree.cpp
+++ src/storage/index/b_plus_tree.cpp
@@ -1,10 +1,16 @@
 #include <string>
 
+#include "common/config.h"
 #include "common/exception.h"
 #include "common/logger.h"
 #include "common/rid.h"
 #include "storage/index/b_plus_tree.h"
+#include "storage/page/b_plus_tree_internal_page.h"
+#include "storage/page/b_plus_tree_leaf_page.h"
+#include "storage/page/b_plus_tree_page.h"
 #include "storage/page/header_page.h"
+#include "storage/page/page.h"
+#include "type/value.h"
 
 namespace bustub {
 INDEX_TEMPLATE_ARGUMENTS
